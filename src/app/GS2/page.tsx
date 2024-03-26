903915915ed7992d5dba0c6f2264a834ea87cd5c
Alexandre De Roeck <Mr.Alexandre.deroeck@gmail.comm>
2024-03-26 15:55:58 +0100

    add test page

---
+++ src/app/GS2/page.tsx
@@ -0,0 +1,195 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <table width="100%"  border={0} cellSpacing={0} cellPadding={15} className="absolute w-[34%] left-[33%]">
+    <tbody>
+      <tr>
+        <td className="yield">
+          <center>
+            <Image
+              src="/web/20230128104123im_/https://v3.goldensun-world.com/img/title/info.gif"
+              width={149}
+              height={55}
+              alt="test"
+            />
+            <br />
+            <br />
+            <table
+              border={1}
+              align="center"
+              width={250}
+              cellSpacing={0}
+              cellPadding={1}
+            >
+              <tbody>
+                <tr>
+                  <td width={100}>
+                    <b>Editeur :</b>
+                  </td>
+                  <td>Nintendo</td>
+                </tr>
+                <tr>
+                  <td>
+                    <b>Concepteur :</b>
+                  </td>
+                  <td>Camelot</td>
+                </tr>
+                <tr>
+                  <td>
+                    <b>Console :</b>
+                  </td>
+                  <td>Game Boy Advance</td>
+                </tr>
+                <tr>
+                  <td>
+                    <b>Genre :</b>
+                  </td>
+                  <td>RPG</td>
+                </tr>
+                <tr>
+                  <td>
+                    <b>Sortie :</b>
+                  </td>
+                  <td>19 Septembre 2003</td>
+                </tr>
+              </tbody>
+            </table>
+          </center>
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Si vous avez adoré{" "}
+          <b>Golden Sun</b> rassurez-vous un second épisode est prévu pour début
+          2003, il s’agit de la suite direct du premier. On l’espère beaucoup plus
+          long et encore plus captivant que le précédent,{" "}
+          <b>Golden Sun 2 : L&lsquo;Âge Perdu</b> devrait être l’un des softs les plus
+          réussi du point de vue graphique sur Game Boy Advance tout en conservant
+          une jouabilité exemplaire.
+          <br />
+          La série Golden Sun restent l’un des meilleurs RPG sortis sur console.
+          Je dirais même LE meilleur.
+          <br />
+          <br />
+          <br />
+          Maintenant un peu d’infos plus approfondies sur le jeu.
+          <br />
+          <br />
+          <table border={0} cellPadding={0} cellSpacing={0}>
+            <tbody>
+              <tr>
+                <td valign="top" width={120}>
+                  <b>Le scénario :</b>
+                </td>
+                <td style={{ textAlign: "justify" }}>
+                  Un scénario très profond puisque que c’est la suite de{" "}
+                  <b>Golden Sun</b>, on y retrouvera{" "}
+                  <b>Vlad, Garet, Ivan, Sofia, Pavel, Lyna, Cylia et Thélos</b>{" "}
+                  ainsi que d’autre personnage de Golden Sun comme Maître Amo et
+                  Alex, mais aussi d’autres ennemis et amis. Il faut dans Golden
+                  Sun 2 allumer les deux autres phares (<b>Jupiter et Mars</b>)
+                  pour terminer le jeu mais il faudra aussi finir des montagnes
+                  aux couleurs différentes et des phare aux couleur différentes
+                  aussi.
+                  <br />
+                  <br />
+                </td>
+              </tr>
+              <tr>
+                <td valign="top">
+                  <b>Les musiques :</b>
+                </td>
+                <td style={{ textAlign: "justify" }}>
+                  On retrouvera certaines musiques de GS et on en entendra des
+                  nouvelles tout aussi enchanteresses et passionnantes.
+                  <br />
+                  <br />
+                </td>
+              </tr>
+              <tr>
+                <td valign="top">
+                  <b>Les graphismes :</b>
+                </td>
+                <td style={{ textAlign: "justify" }}>
+                  Pas de changement, le mode aventure reste le même que dans
+                  L&lsquo;opus précédent, rien à redire.
+                  <br />
+                  <br />
+                  En combat, le même mode 3D est conservé, on passe à une vue 360°
+                  particulièrement détaillé, la caméra est derrière les persos
+                  lorsque L&lsquo;on choisit ses attaques puis bouge tout autour des
+                  ennemis tout en gardant une excellente fluidité, les invocations
+                  sont particulièrement impressionnantes.
+                  <br />
+                  <br />
+                </td>
+              </tr>
+              <tr>
+                <td valign="top">
+                  <b>La jouabilité :</b>
+                </td>
+                <td style={{ textAlign: "justify" }}>
+                  Excellente, on s’habitue rapidement aux boutons que ce soit hors
+                  combat ou en combat.
+                  <br />
+                  <br />
+                </td>
+              </tr>
+              <tr>
+                <td valign="top">
+                  <b>Les inovations :</b>
+                </td>
+                <td style={{ textAlign: "justify" }}>
+                  Bien que le système du jeu soit identique à celui du premier
+                  volume, on voit L&lsquo;apparitions de multiples nouveautés : de
+                  nouvelles invocations, de nouveaux djinn et de nouveaux héros.
+                  Vous pourrez aussi naviguer à bord d&lsquo;un bateau volant !!
+                  <br />
+                  <br />
+                </td>
+              </tr>
+              <tr>
+                <td valign="top">
+                  <b>Le final :</b>
+                </td>
+                <td style={{ textAlign: "justify" }}>
+                  Si vous avez été fou de rage ou si vous avez failli démolir
+                  votre GBA quand vous avez lu “à suivre” dans GS1, il ne vous
+                  reste plus qu’à vous précipitez pour aller acheter la suite et
+                  connaître ainsi la fin de L&lsquo;histoire, puisque GS2 est tout à
+                  fait exceptionnel aussi bien pour son scénario que ses
+                  graphismes révolutionnaires sur GBA, le tout accompagné de
+                  superbes musiques. Les Pokémon peuvent se retourner dans leur
+                  tombe. &gt;:-D
+                  <br />
+                  <br />
+                  <br />
+                </td>
+              </tr>
+              <tr>
+                <td valign="top">
+                  <b>Note Générale :</b>
+                </td>
+                <td>
+                    <b>
+                      <div className="flex justify-end">18/20</div>
+                    </b>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <br />
+          <br />
+          <br />
+          <br />
+          <div align="right">Preview réalisée par Cristali feat. Garyash</div>
+          <br />
+          <br />
+          <br />
+          <br />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  
+  );
+}
