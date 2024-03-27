d48874487392f9df61efd77de5cf0ffd34389462
Alexandre De Roeck <Mr.Alexandre.deroeck@gmail.comm>
2024-03-27 18:16:37 +0100

    add info and histoire page

---
+++ src/app/GS2/histoire/page.tsx
@@ -0,0 +1,276 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+
+<table width="100%"   cellSpacing={0} cellPadding={15} className="absolute w-[34%] left-[33%]">
+  <tbody>
+    <tr>
+      <td className="yield">
+        <table
+          width={420}
+          border={0}
+          cellPadding={0}
+          cellSpacing={0}
+          align="center"
+        >
+          <tbody>
+            <tr>
+              <td>
+                <center>
+                  <Image
+                    src="/web/20230128095454im_/https://v3.goldensun-world.com/img/title/histoire.gif"
+                    width={168}
+                    height={45}
+                    alt="test"
+                  />
+                </center>
+                  <br />
+                  <br />
+                  &nbsp;&nbsp;
+                  <b>
+                    <u>
+                        Prologue du Livre 1 (Golden Sun : Le Sceau Brisé):
+                    </u>
+                  </b>
+                  <br />
+                  <br />
+                  <br />
+                  <b>Alchimie</b> : Science occulte à la recherche d&lsquo;un remède
+                  universel capable d&lsquo;opérer une transmutation de l&lsquo;être et de
+                  la matière.
+                    © Larousse.
+                  <br />
+                  <br />
+                  <br />
+                  <i>
+                    Richesses infinies, vie éternelle, conquêtes et pouvoir
+                    suprême. Tels furent les ambitions des hommes face à cette
+                    puissance. Afin d&lsquo;éviter une guerre amenant à la destruction
+                    du monde, les sages enfouirent les secrets de l&lsquo;
+                    <b className="text-[#0000cc]" >
+                      Alchimie
+                    </b>{" "}
+                    dans le <b>temple de Sol</b>, édifice caché dans le{" "}
+                    <b>Mont Alpha</b> et protégé par le village de <b>Val</b>.
+                  </i>
+                  <br />
+                  <br />
+                  <i>
+                    Mais un jour, deux êtres étranges nommés{" "}
+                    <b className="text-[#DD0000]">
+                      Salamandar
+                    </b>{" "}
+                    et{" "}
+                    <b className="text-[#DD0000]">
+                    Phoenixia
+                    </b>{" "}
+                    tentèrent de s&lsquo;approprier les{" "}
+                    <b className="text-[#009900]">
+                      étoiles élémentaires
+                    </b>{" "}
+                    qui maintenaient l&lsquo;équilibre de l&lsquo;
+                    <b className="text-[#0000cc]">
+                      Alchimie
+                    </b>
+                    . Ils échouèrent et <b>Val</b> fut pris dans un cataclysme
+                    qui coûta la vie à de nombreuses personnes, dont le père du
+                    jeune{" "}
+                    <b className="text-[#663300]" >
+                        Vlad
+                    </b>{" "}
+                    et la famille de{" "}
+                    <b className="text-[#DD0000]">
+                        Lina
+                    </b>
+                    .
+                  </i>
+                  <br />
+                  <br />
+                  <br />
+                  &nbsp;&nbsp;
+                  <u>
+                    <b>
+                        Golden Sun <i>(</i>黄金の太陽　開かれし封印<i>)</i>
+
+                    </b>
+                  </u>
+                  <br />
+                  <br />
+                  <br />3 ans plus tard :<br />
+                  <br />
+                  <p style={{ textAlign: "justify" }}>
+                    <Image
+                      src="/web/20230128095454im_/https://v3.goldensun-world.com/img/screen/gs1_histoire1_mini.png"
+                      width={120}
+                      height={65}
+                      alt="test"
+                    />
+                    <b  className="text-[#663300]">
+                        Vlad
+                    </b>{" "}
+                    ainsi que ses amis d&lsquo;enfance{" "}
+                    <b className="text-[#DD0000]">
+                      Garet
+                    </b>{" "}
+                    et{" "}
+                    <b className="text-[#DD0000]">
+                        Lina
+                    </b>{" "}
+                    décident d&lsquo;accompagner <b>Thélos</b>, un savant venu étudier
+                    et explorer le temple de Sol, sur le <b>Mont Alpha</b>.
+                    Alors que leur expédition touchait à son but, un groupe de
+                    quatre personnes composé de{" "}
+                    <b className="text-[#DD0000]">
+                        Salamandar
+                    </b>
+                    ,{" "}
+                    <b className="text-[#DD0000]">
+                      Phoenixia
+                    </b>
+                    ,{" "}
+
+                      <b className="text-[#0066cc]" >Alex</b>
+                        {" "}
+                    et{" "}
+                    <b className="text-[#663300]" >
+                        Pavel 
+                    </b>
+                    , le frère de{" "}
+                    <b className="text-[#DD0000]">
+                     Lina
+                    </b>{" "}
+                    supposé avoir disparu il y a 3 ans, vint leur barrer la
+                    route. Ils s&lsquo;emparèrent des{" "}
+                    <b  className="text-[#009900]">
+                        étoiles élémentaires
+                    </b>
+                    , qui permettent d&lsquo;allumer les quatre{" "}
+                    <b  className="text-[#009900]">
+                        phares élémentaires
+                    </b>{" "}
+                    et ainsi libérer la puissance scellée de l&lsquo;
+                    <b>
+                        Alchimie
+                    </b>
+                    , et prirent en otage <b>Thélos</b> et{" "}
+                    <b className="text-[#DD0000]">
+                        Lina
+                    </b>
+                    .
+                  </p>
+                  <br />
+                  Partis à leur poursuite avec pour mission d&lsquo;empêcher
+                  l&lsquo;activation des phares,{" "}
+                  <b  className="text-[#663300]">
+                    Vlad
+                  </b>{" "}
+                  et{" "}
+                  <b className="text-[#DD0000]">
+                    Garet
+                  </b>{" "}
+                  rencontrèrent{" "}
+
+                    <b>Ivan</b>
+                        {" "}
+                  puis{" "}
+
+                    <b className="text-[#0066cc]" >Sofia</b>
+
+                  , qui se joignirent tout deux à leur quête. Malgré les effort
+                  du groupe,{" "}
+                  <b className="text-[#DD0000]">
+                    Salamandar
+                  </b>{" "}
+                  et{" "}
+                  <b className="text-[#DD0000]">
+                    Phoenixia
+                  </b>{" "}
+                  parvinrent à allumer les phares de{" "}
+ 
+                    <b className="text-[#0066cc]">Mercure</b>
+                    {" "}
+                  et de{" "}
+                  <b  className="text-[#663300]">
+                    Vénus
+                  </b>
+                  , mais périrent après avoir activé ce dernier.
+                  <br />
+                  <br />À la suite de l&lsquo;activation du Phare de{" "}
+                  <b  className="text-[#663300]">
+                Vénus
+                  </b>
+                  , un nouveau cataclysme se produisit et{" "}
+  
+                    <b className="text-[#9966ff]">Cylia</b>
+
+                  , une autre otage, ainsi que{" "}
+                  <b  className="text-[#663300]">
+                    Pavel
+                  </b>{" "}
+                  tombèrent dans la mer et furent emportés. Ils se réveillèrent
+                  entourés par{" "}
+
+                    <b className="text-[#0066cc]">Alex</b>
+
+                  , <b>Thélos</b> et{" "}
+                  <b className="text-[#DD0000]">
+                    Lina
+                  </b>
+                  , sur une île dérivant sur l&lsquo;Océan.
+                  <br />
+                  <br />
+                  <br />
+                  &nbsp;&nbsp;
+                  <b className="text-[#DD0000]">
+                      <u>
+                        Golden Sun : l&lsquo;Âge Perdu <i>(</i>
+                        黄金の太陽　失われし時代<i>)</i>
+                      </u>
+                  </b>
+                  <br />
+                  <br />
+                  Pour une raison encore mystérieuse,{" "}
+                  <b  className="text-[#663300]">
+                    Pavel
+                  </b>
+                  ,{" "}
+                  <b className="text-[#DD0000]">
+                    Lina
+                  </b>
+                  ,{" "}
+                    <b className="text-[#9966ff]">Cylia</b>
+                    {" "}
+                  et <b>Thélos</b> sont déterminés à allumer les phares restant,
+                  et ce malgré{" "}
+                  <b  className="text-[#663300]">
+                    Vlad
+                  </b>{" "}
+                  et ses amis à leur poursuite.
+                  <br />
+                  Quelle raison donner à cette quête ? Qu&lsquo;arrivera-t-il lorsque
+                  les derniers phares s&lsquo;allumeront ?<br />À vous de le découvrir
+                  dans <b>Golden Sun : l&lsquo;Âge perdu</b> !
+                  <br />
+                  <br />
+                  <center>
+                    <Image
+                      src="/web/20230128095454im_/https://v3.goldensun-world.com/img/screen/gs2_histoire_anim.gif"
+                      width={240}
+                      height={160}
+                        alt="test"
+                    />
+                  </center>
+                  <br />
+
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  </tbody>
+</table>
+  );
+}
